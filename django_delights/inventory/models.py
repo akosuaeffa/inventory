11a8f19745b595e53476ccffc225d14672d7087e
akosuaeffa <akosuaeffah24@gmail.com>
2024-06-22 11:39:34 +0000

    created models

---
+++ django_delights/inventory/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+#model for ingredients
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.FloatField()
+    price_per_unt = models.FloatField()
+
+    def __str_(self):
+        return self.name
+
+# model for the meniu
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+
+# model for Recipe
+class RecipeRequirementI(models.Model):
+    menu = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quanity = models.FloatField()
+
+    def __str__(self):
+        return f"{self.quanity} of {self.ingredient.name} for {self.menu.name}"
+    
+
+#model for purchases
+class Purchase(models.Model):
+    menu = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Purchase of {self.menu.name} at {self.timestamp}"
+
